cbc23bab5b56e571a93e3a38daf9306538157053
masashimorita <masashi.morita.mm@gmail.com>
2025-01-06 12:00:57 +0900

    :sparkles: Implemented an agent to scrape twitter account & recent tweets

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,28 +4,36 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 
 
 def ice_break_with(name: str, mock: bool = False) -> str:
-    linkedin_username = linkedin_lookup_agent(name)
+    linkedin_username = linkedin_lookup_agent(name=name)
     print(linkedin_username)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=mock)
 
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, mock=mock)
+    print(twitter_username)
+    print(tweets)
+
     summary_tempalte = """
-        given the LinkedIn information {information} about a person from I want you to create:
+        given the information about a person from LinkedIn {information},
+        and their latest twitter posts {twitter_posts} I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_tempalte
+        input_variables=["information", "twitter_posts"], template=summary_tempalte
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    
+
     chain = summary_prompt_template | llm | StrOutputParser()
-    res = chain.invoke(input={"information": linkedin_data})
+    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
     print(res)
 
