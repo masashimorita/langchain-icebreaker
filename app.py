d9922786099f1709d78cb6a000d5d7949ef78134
masashimorita <masashi.morita.mm@gmail.com>
2025-01-06 20:24:31 +0900

    :sparkles: Plug in ice_break_with to frontend to make it work in real

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
-print("hello pipenv-docker-development world!", flush=True)
+from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+from ice_breaker import ice_break_with
 
-print("shutdown...", flush=True)
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/process", methods=["POST"])
+def process():
+    name = request.form["name"]
+    summary, profile_pic_url = ice_break_with(name=name)
+
+    return jsonify(
+        {
+            "summary_and_facts": summary.to_dict(),
+            "picture_url": profile_pic_url
+        }
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
