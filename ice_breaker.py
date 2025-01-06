f4de0de74d791c9678ff4aceaf635710430eca88
masashimorita <masashi.morita.mm@gmail.com>
2025-01-06 19:52:52 +0900

    :sparkles: Implemented an Output Parser object with pydantic package

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,6 +7,7 @@ from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import summary_parser
 
 
 def ice_break_with(name: str, mock: bool = False) -> str:
@@ -24,15 +25,19 @@ def ice_break_with(name: str, mock: bool = False) -> str:
         and their latest twitter posts {twitter_posts} I want you to create:
         1. a short summary
         2. two interesting facts about them
+
+        Use both information from twitter and Linkedin
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information", "twitter_posts"], template=summary_tempalte
+        input_variables=["information", "twitter_posts"], template=summary_tempalte,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
     print(res)
